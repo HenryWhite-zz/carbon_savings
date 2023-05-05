08a91f970f68fb62f8ea1abb52a7009a1ba79303
Henry White <henry.white@gmail.com>
2023-05-04 22:11:27 -0600

    Add support for state and BA inputs

--- etl/etl_pandas.py
+++ etl/etl_pandas.py
@@ -1,10 +1,35 @@
+import requests
+import io
+from zipfile import ZipFile
 import pandas as pd
+import os
 
-def extract_data(file_path):
-    # Read in the Excel file using pandas
-    data = pd.read_excel(file_path)
+def extract_excel_zip(url, file):
+    # Extract excel file from a zipped folder
+    # Check if it already exists
+    if not os.path.isfile(f'data/source/{file}'):
+        r = requests.get(url)
+        zip_file = ZipFile(io.BytesIO(r.content))
+        zip_file.extract(file, 'data/source')
+
+    # Create dataframe from excel file
+    data = pd.read_excel(f'data/source/{file}')
     return data
 
-def transform_data(data):
-    # Apply some transformations to the data using pandas
-    return data
+
+def transform_ba(df):
+    df.columns=['year', 'ba_id', 'ba_code', 'state', 'ba_name']
+    return df
+
+# Temporarily calling functions for easy verification
+"""
+df = extract_excel_zip('https://www.eia.gov/electricity/data/eia861/zip/f8612021.zip',
+                  'Balancing_Authority_2021.xlsx')
+
+df = transform_ba(df)
+
+print(df)
+
+df2 = df[df['state'] == 'MT']['ba_name']
+print(df2)
+"""
